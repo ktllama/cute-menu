42d9a6002af9ef36ab29df2c2ff02b4365b9bb78
Katie <costantinkp@gmail.com>
2021-10-14 15:39:01 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+
+(function () {
+    "use strict";
+
+    const box = document.getElementById('btn');
+    const img = document.querySelector('img');
+
+    box.addEventListener('mouseover', function() {
+        box.className = "phoneicon";
+        box.innerHTML = "1-800-OUR-MENU";
+    });
+
+    box.addEventListener('mouseout', function() {
+        box.className = "button";
+        box.innerHTML ='<img src="2.png" class="img" alt="phone" width="100em">';
+    });
+
+})();
